e037848b80c7d9682a1431186df3d2ea45d6a000
Matt Traudt <sirmatt@ksu.edu>
2017-01-29 20:26:29 -0500

    Should've tested more. Now actually holds a temperature

--- usability/screen-color.py
+++ usability/screen-color.py
@@ -20,32 +20,39 @@ from datetime import datetime, time, timedelta
 min_temp = 2000
 max_temp = 6500
 # Can't transition across midnight, sorry
-transitions = [
+periods = [
+    ('min',time(0,0),time(6,0)),
     ('up',time(6,0),time(9,0)),
+    ('max',time(9,0),time(17,0)),
     ('down',time(17,0),time(20,0)),
+    ('min',time(20,0),time(23,59)),
 ]
 ################################################################################
 
 now = datetime.now().time()
 
-for trans in transitions:
-    direction, start, end = trans
+for period in periods:
+    direction, start, end = period
     if now >= start and now < end:
-        temp_diff = max_temp - min_temp
-        seconds_since_start = \
-            (now.hour - start.hour) * 3600 + \
-            (now.minute - start.minute) * 60 + \
-            (now.second - start.second)
-        total_seconds = \
-            (end.hour - start.hour) * 3600 + \
-            (end.minute - start.minute) * 60 + \
-            (end.second - start.second)
-        factor = seconds_since_start / total_seconds
-        amount = temp_diff * factor
-        if direction == 'up':
-            new_temp = min_temp + amount
+        if direction == 'min':
+            new_temp = min_temp
+        elif direction == 'max':
+            new_temp = max_temp
         else:
-            new_temp = max_temp - amount
+            temp_diff = max_temp - min_temp
+            seconds_since_start = \
+                (now.hour - start.hour) * 3600 + \
+                (now.minute - start.minute) * 60 + \
+                (now.second - start.second)
+            total_seconds = \
+                (end.hour - start.hour) * 3600 + \
+                (end.minute - start.minute) * 60 + \
+                (end.second - start.second)
+            factor = seconds_since_start / total_seconds
+            amount = temp_diff * factor
+            if direction == 'up':
+                new_temp = min_temp + amount
+            elif direction == 'down':
+                new_temp = max_temp - amount
         new_temp = int(new_temp)
         print(new_temp)
-        break
